3af9fd7bab3b93727e84ff292d9aa74c526f0f57
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-08-30 15:44:33 +0300

    Added Employee and Order models

--- producer_consumer/models.py
+++ producer_consumer/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Employee(AbstractUser):
+    probation = models.BooleanField(default=False)
+    position = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "employee"
+        verbose_name_plural = "employees"
+
+
+class Order(models.Model):
+    task_id = models.IntegerField()
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
