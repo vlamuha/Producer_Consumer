74c83924a01bffce853129caf4af62ac87744eb5
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-08-30 15:49:43 +0300

    Blacked

--- ProdCons/wsgi.py
+++ ProdCons/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProdCons.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProdCons.settings")
 
 application = get_wsgi_application()
--- producer_consumer/apps.py
+++ producer_consumer/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ProducerConsumerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'producer_consumer'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "producer_consumer"
